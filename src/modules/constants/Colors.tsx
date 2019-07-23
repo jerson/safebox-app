a308a32e527610acbfe734c3570d755e0c0d062e
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-07-23 12:36:53 -0500

    added client with promise suport

---
+++ src/modules/constants/Colors.tsx
@@ -0,0 +1,19 @@
+export default {
+  primary: '#00964B',
+  primaryLight: '#71B74C',
+  secondary: '#3FB4E5',
+  shine: '#FFD100',
+  accentDark: '#F28F00',
+  accent: '#FAB500',
+  danger: '#D4192F',
+
+  grey6: '#434A54',
+  grey5: '#656D78',
+  grey4: '#AAB2BD',
+  grey3: '#CCD1D9',
+  grey2: '#E6E9ED',
+  grey1: '#F7F9FC',
+  white: '#FFFFFF',
+  black: '#000000',
+  transparent: 'transparent'
+};
---
+++ src/services/Client.tsx
@@ -0,0 +1,94 @@
+import { ServicesClient } from '../proto/ServicesServiceClientPb';
+import Config from '../Config';
+import {
+  AccountRequest,
+  AccountResponse,
+  AccountsRequest,
+  AccountsResponse,
+  AddAccountRequest,
+  AddAccountResponse,
+  BuyProductRequest,
+  BuyProductResponse,
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+  RegisterResponse
+} from '../proto/services_pb';
+
+export default class Client {
+  static login(request: LoginRequest): Promise<LoginResponse> {
+    const client = this.connect();
+    return new Promise((resolve, reject) => {
+      client.login(request, null, (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  static register(request: RegisterRequest): Promise<RegisterResponse> {
+    const client = this.connect();
+    return new Promise((resolve, reject) => {
+      client.register(request, null, (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  static addAccount(request: AddAccountRequest): Promise<AddAccountResponse> {
+    const client = this.connect();
+    return new Promise((resolve, reject) => {
+      client.addAccount(request, null, (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  static getAccount(request: AccountRequest): Promise<AccountResponse> {
+    const client = this.connect();
+    return new Promise((resolve, reject) => {
+      client.getAccount(request, null, (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  static getAccounts(request: AccountsRequest): Promise<AccountsResponse> {
+    const client = this.connect();
+    return new Promise((resolve, reject) => {
+      client.getAccounts(request, null, (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  static buyProduct(request: BuyProductRequest): Promise<BuyProductResponse> {
+    const client = this.connect();
+    return new Promise((resolve, reject) => {
+      client.buyProduct(request, null, (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  private static connect() {
+    return new ServicesClient(Config.server.url);
+  }
+}
