39e193d0d9078e10ae5e46f06b657e4e910ed28c
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-07-24 22:03:52 -0500

    added transition fade for login stack

--- src/modules/constants/NavigationOptions.tsx
+++ src/modules/constants/NavigationOptions.tsx
@@ -7,8 +7,52 @@ import {
 } from 'react-navigation';
 import BackIcon from '../../components/navigation/BackIcon';
 import Header from '../../components/navigation/Header';
-import { View } from 'react-native';
+import { View, Easing, Animated } from 'react-native';
 
+const transitionConfigSlide = () => {
+  return {
+    transitionSpec: {
+      duration: 750,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true
+    },
+    screenInterpolator: (sceneProps: any) => {
+      const { layout, position, scene } = sceneProps;
+
+      const thisSceneIndex = scene.index;
+      const width = layout.initWidth;
+
+      const translateX = position.interpolate({
+        inputRange: [thisSceneIndex - 1, thisSceneIndex],
+        outputRange: [width, 0]
+      });
+
+      return { transform: [{ translateX }] };
+    }
+  };
+};
+const transitionConfigFade = () => {
+  return {
+    transitionSpec: {
+      duration: 750,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true
+    },
+    screenInterpolator: (sceneProps: any) => {
+      const { position, scene } = sceneProps;
+
+      const thisSceneIndex = scene.index;
+      const opacity = position.interpolate({
+        inputRange: [thisSceneIndex - 1, thisSceneIndex],
+        outputRange: [0, 1]
+      });
+
+      return { opacity };
+    }
+  };
+};
 const header: StackNavigatorConfig = {
   headerMode: 'screen',
   defaultNavigationOptions: ({
@@ -35,7 +79,9 @@ const header: StackNavigatorConfig = {
 };
 
 const NavigationOptions = {
-  header
+  header,
+  transitionConfigFade,
+  transitionConfigSlide
 };
 
 export default NavigationOptions;
