05d30ebe4be2c4a7496fccf71a5ca27506db1fc9
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-07-25 19:06:52 -0500

    added remove account

--- src/scenes/app/AccountScreen.tsx
+++ src/scenes/app/AccountScreen.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   View,
   ViewStyle,
   ScrollView,
-  StatusBar
+  StatusBar,
+  Alert
 } from 'react-native';
 import { SafeAreaView, HeaderProps } from 'react-navigation';
 import Colors from '../../modules/constants/Colors';
@@ -12,12 +13,18 @@ import Colors from '../../modules/constants/Colors';
 import Container from '../../components/ui/Container';
 import Content from '../../components/ui/Content';
 import Size from '../../modules/dimensions/Size';
-import { AccountSingle } from '../../proto/services_pb';
-import { useNavigationParam } from 'react-navigation-hooks';
+import { AccountSingle, DeleteAccountRequest } from '../../proto/services_pb';
+import { useNavigationParam, useNavigation } from 'react-navigation-hooks';
 import Text from '../../components/ui/Text';
 import Header from '../../components/account/Header';
 import useIconTintLabel from '../../components/hooks/useIconTintLabel';
 import Session from '../../services/Session';
+import HeaderIcon from '../../components/navigation/HeaderIcon';
+import LoadingOverlay from '../../components/ui/LoadingOverlay';
+import Client from '../../services/Client';
+import AlertMessage from '../../components/ui/AlertMessage';
+import useAnimatedState from '../../components/hooks/useAnimatedState';
+import Strings from '../../modules/format/Strings';
 
 const styles = StyleSheet.create({
   container: {
@@ -37,11 +44,59 @@ const styles = StyleSheet.create({
 });
 
 function AccountScreen() {
+  const { setParams, goBack } = useNavigation();
   const account = useNavigationParam('account') as AccountSingle;
+  const showDelete = useNavigationParam('showDelete') as boolean;
+
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [error, setError] = useAnimatedState('');
+
+  const deleteAccount = async () => {
+    setIsDeleting(true);
+    try {
+      const request = new DeleteAccountRequest();
+      request.setId(account.getId());
+      await Client.deleteAccount(request);
+      goBack();
+    } catch (e) {
+      const message = Strings.getError(e);
+      setError(message);
+    }
+    setIsDeleting(false);
+  };
+
+  useEffect(() => {
+    if (!showDelete) {
+      return;
+    }
+    Alert.alert(
+      'Delete secret account',
+      'Are you sure?',
+      [
+        {
+          text: 'Delete',
+          onPress: () => {
+            deleteAccount();
+            setParams({ showDelete: false });
+          }
+        },
+        {
+          text: 'Cancel',
+          onPress: () => {
+            setParams({ showDelete: false });
+          },
+          style: 'cancel'
+        }
+      ],
+      { cancelable: false }
+    );
+  }, [showDelete]);
+
   const [hasPassword] = useState(!!Session.getPassword());
 
   return (
     <Container style={styles.container}>
+      <LoadingOverlay isLoading={isDeleting} />
       <StatusBar
         animated
         barStyle={'dark-content'}
@@ -56,6 +111,7 @@ function AccountScreen() {
       >
         <SafeAreaView style={styles.safeArea}>
           <Content center>
+            {!!error && <AlertMessage message={error} />}
             {hasPassword && (
               <View style={styles.form}>
                 <Text>{account.getId()}dd</Text>
@@ -85,7 +141,15 @@ AccountScreen.navigationOptions = ({ navigation }: any) => ({
         {...props}
       />
     );
-  }
+  },
+  headerRight: (
+    <HeaderIcon
+      name={'trash-2'}
+      onPress={() => {
+        navigation.setParams({ showDelete: true });
+      }}
+    />
+  )
 });
 
 export default AccountScreen;
