9be82b2d72a1e594890e0bf6fe0cc63b1db99c86
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-07-28 23:06:09 -0500

    removed codepush from debug

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { StatusBar, Platform, UIManager } from 'react-native';
 import MainNavigator from './router';
 import Colors from './modules/constants/Colors';
 const tinyColor = require('tinycolor2');
+import codePush from 'react-native-code-push';
 
 if (Platform.OS === 'android') {
   UIManager.setLayoutAnimationEnabledExperimental &&
     UIManager.setLayoutAnimationEnabledExperimental(true);
 }
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
+};
 
 function App() {
   const statusBarColor = tinyColor(Colors.primary).darken(5);
@@ -24,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default codePush(codePushOptions)(App);
