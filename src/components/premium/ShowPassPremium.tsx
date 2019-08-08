9721ee1024d5abf2dee8dcadd50b70f35d130157
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-08-07 21:41:02 -0500

    refactored itemPremium

--- src/components/premium/ShowPassPremium.tsx
+++ src/components/premium/ShowPassPremium.tsx
@@ -1,147 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  ViewStyle,
-  View,
-  StyleProp,
-  TextStyle,
-  Platform
-} from 'react-native';
-import Colors from '../../modules/constants/Colors';
-import SplitText from '../ui/SplitText';
-import Text from '../ui/Text';
-import Icon from 'react-native-vector-icons/Feather';
-import Client from '../../services/Client';
-import { HasProductRequest, BuyProductRequest } from '../../proto/services_pb';
-import Strings from '../../modules/format/Strings';
-import AlertMessage from '../ui/AlertMessage';
-import Button from '../ui/Button';
-import { useNavigation } from 'react-navigation-hooks';
-import useFocusedScreen from '../hooks/useFocusedScreen';
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.grey1
-  } as ViewStyle,
-  content: {
-    marginTop: 10,
-    flexDirection: 'row',
-    alignItems: 'center'
-  } as ViewStyle,
-  info: {
-    flex: 1,
-    paddingRight: 20
-  } as ViewStyle,
-  description: {
-    marginBottom: 10,
-    fontSize: 13,
-    color: Colors.grey5
-  } as TextStyle,
-  icon: {
-    fontSize: 28,
-    marginRight: 15,
-    marginLeft: 10,
-    color: Colors.grey5
-  } as TextStyle,
-  splitText: {
-    marginBottom: 5
-  } as ViewStyle
-});
+import React from 'react';
+import { ViewStyle, StyleProp } from 'react-native';
+import ItemPremium from './ItemPremium';
 
 export interface ShowPassPremiumProps {
   style?: StyleProp<ViewStyle>;
 }
 
-const productId = 'showpass';
-
 function ShowPassPremium({ style }: ShowPassPremiumProps) {
-  const [isPurchased, setIsPurchased] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const navigation = useNavigation();
-  const [focused] = useFocusedScreen(navigation);
-
-  useEffect(() => {
-    focused && check();
-  }, [focused]);
-
-  const check = async () => {
-    try {
-      const request = new HasProductRequest();
-      request.setSlug(productId);
-      const response = await Client.hasProduct(request);
-      setIsPurchased(response.getPurchased());
-    } catch (e) {
-      const message = Strings.getError(e);
-      setError(message);
-    }
-
-    setIsLoading(false);
-  };
-
-  const purchase = async () => {
-    if (isPurchased) {
-      return;
-    }
-    setIsLoading(true);
-    try {
-      const request = new BuyProductRequest();
-      request.setSlug(productId);
-      request.setPayload('sample');
-      request.setType(Platform.OS);
-      await Client.buyProduct(request);
-      setIsPurchased(true);
-    } catch (e) {
-      const message = Strings.getError(e);
-      setError(message);
-      check();
-    }
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    check();
-  }, []);
-
-  const price = 'S/ 1.00';
-  const buttonTitle = isPurchased ? 'Purchased' : `Purchase Now - ${price}`;
   return (
-    <View style={[styles.container, style]}>
-      <SplitText
-        style={styles.splitText}
-        title={'Show Password - Premium'}
-        type={'Default'}
-      />
-
-      {!!error && (
-        <AlertMessage
-          onTimeout={() => {
-            setError('');
-          }}
-          message={error}
-        />
-      )}
-      <View style={styles.content}>
-        <Icon name={'eye'} style={[styles.icon]} />
-        <View style={styles.info}>
-          <Text style={styles.description}>
-            With this option add additional security to your account and enable
-            the option to see the passwords your secret accounts.
-          </Text>
-          <Button
-            icon={isPurchased ? undefined : 'star'}
-            typeColor={'primaryLight'}
-            onPress={purchase}
-            disabled={isPurchased}
-            isLoading={isLoading}
-            title={isLoading ? undefined : buttonTitle}
-          />
-        </View>
-      </View>
-    </View>
+    <ItemPremium
+      style={style}
+      productId={'showpass'}
+      name={'Show Password'}
+      icon={'eye'}
+      description={`With this option add additional security to your account and enable the option to see the passwords your secret accounts.`}
+    />
   );
 }
 
--- src/components/premium/TrackPhonePremium.tsx
+++ src/components/premium/TrackPhonePremium.tsx
@@ -1,79 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  ViewStyle,
-  View,
-  StyleProp,
-  TextStyle,
-  Platform
-} from 'react-native';
+import { ViewStyle, View, StyleProp } from 'react-native';
 import Colors from '../../modules/constants/Colors';
-import SplitText from '../ui/SplitText';
 import Text from '../ui/Text';
-import Icon from 'react-native-vector-icons/Feather';
 import Client from '../../services/Client';
-import {
-  HasProductRequest,
-  BuyProductRequest,
-  EnableLocationRequest
-} from '../../proto/services_pb';
+import { EnableLocationRequest } from '../../proto/services_pb';
 import Strings from '../../modules/format/Strings';
-import AlertMessage from '../ui/AlertMessage';
 import Button from '../ui/Button';
 import TextInput from '../ui/TextInput';
-import { useNavigation } from 'react-navigation-hooks';
-import useFocusedScreen from '../hooks/useFocusedScreen';
 import useTextInput from '../hooks/useTextInput';
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.grey1
-  } as ViewStyle,
-  content: {
-    marginTop: 10,
-    flexDirection: 'row',
-    alignItems: 'center'
-  } as ViewStyle,
-  info: {
-    flex: 1,
-    paddingRight: 20
-  } as ViewStyle,
-  description: {
-    marginBottom: 10,
-    fontSize: 13,
-    color: Colors.grey5
-  } as TextStyle,
-  icon: {
-    fontSize: 28,
-    marginRight: 15,
-    marginLeft: 10,
-    color: Colors.grey5
-  } as TextStyle,
-  splitText: {
-    marginBottom: 5
-  } as ViewStyle
-});
+import ItemPremium from './ItemPremium';
+import AlertMessage from '../ui/AlertMessage';
 
 export interface TrackPhonePremiumProps {
   style?: StyleProp<ViewStyle>;
 }
 
-const productId = 'trackphone';
-
 function TrackPhonePremium({ style }: TrackPhonePremiumProps) {
   const [isPurchased, setIsPurchased] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
+  const [emailInput, emailInputProps] = useTextInput('');
   const [error, setError] = useState('');
 
-  const [emailInput, emailInputProps] = useTextInput('');
-
-  const navigation = useNavigation();
-  const [focused] = useFocusedScreen(navigation);
-
-  useEffect(() => {
-    focused && check();
-  }, [focused]);
+  const onPurchase = (isPurchased: boolean) => {
+    setIsPurchased(isPurchased);
+  };
 
   const loadEmail = async () => {
     try {
@@ -92,46 +42,6 @@ function TrackPhonePremium({ style }: TrackPhonePremiumProps) {
     }
     loadEmail();
   }, [isPurchased]);
-
-  const check = async () => {
-    try {
-      const request = new HasProductRequest();
-      request.setSlug(productId);
-      const response = await Client.hasProduct(request);
-      setIsPurchased(response.getPurchased());
-    } catch (e) {
-      const message = Strings.getError(e);
-      setError(message);
-    }
-
-    setIsLoading(false);
-  };
-
-  const purchase = async () => {
-    if (isPurchased) {
-      return;
-    }
-    setIsLoading(true);
-    try {
-      const request = new BuyProductRequest();
-      request.setSlug(productId);
-      request.setPayload('sample');
-      request.setType(Platform.OS);
-      await Client.buyProduct(request);
-      setIsPurchased(true);
-    } catch (e) {
-      const message = Strings.getError(e);
-      setError(message);
-      check();
-    }
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    check();
-  }, []);
 
   const disable = async () => {
     const tmpEmail = email;
@@ -157,15 +67,15 @@ function TrackPhonePremium({ style }: TrackPhonePremiumProps) {
       setEmail('');
     }
   };
-  const price = 'S/ 1.00';
-  const buttonTitle = isPurchased ? 'Purchased' : `Purchase Now - ${price}`;
   return (
-    <View style={[styles.container, style]}>
-      <SplitText
-        style={styles.splitText}
-        title={'Track Phone - Premium'}
-        type={'Default'}
-      />
+    <ItemPremium
+      style={style}
+      onPurchase={onPurchase}
+      productId={'trackphone'}
+      name={'Track Phone'}
+      icon={'map-pin'}
+      description={`If you enable this option we will send you an email daily with the last location you used to connect to the application.`}
+    >
       {!!error && (
         <AlertMessage
           onTimeout={() => {
@@ -174,24 +84,6 @@ function TrackPhonePremium({ style }: TrackPhonePremiumProps) {
           message={error}
         />
       )}
-      <View style={styles.content}>
-        <Icon name={'map-pin'} style={styles.icon} />
-        <View style={styles.info}>
-          <Text style={styles.description}>
-            If you enable this option we will send you an email daily with the
-            last location you used to connect to the application.
-          </Text>
-          <Button
-            icon={isPurchased ? undefined : 'star'}
-            typeColor={'primaryLight'}
-            onPress={purchase}
-            disabled={isPurchased}
-            isLoading={isLoading}
-            title={isLoading ? undefined : buttonTitle}
-          />
-        </View>
-      </View>
-
       {isPurchased && (
         <View
           style={{
@@ -267,7 +159,7 @@ function TrackPhonePremium({ style }: TrackPhonePremiumProps) {
           )}
         </View>
       )}
-    </View>
+    </ItemPremium>
   );
 }
 
