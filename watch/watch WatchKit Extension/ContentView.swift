be73ea170c72c7d1ac73c30e06f3e1964f8d5764
jeral17@gmail.com <Gerson Alexander Pardo Gamez>
2019-08-13 09:07:34 -0700

    added watch project

---
+++ watch/watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watch WatchKit Extension
+//
+//  Created by Gerson Alexander Pardo Gamez on 8/2/19.
+//  Copyright © 2019 Gerson Alexander Pardo Gamez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        LoginView()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
