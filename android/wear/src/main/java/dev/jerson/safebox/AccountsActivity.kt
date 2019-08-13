47ef41fd46356f1e2a9b41d004c81f85a126ccf4
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-08-12 22:30:54 -0500

    merged with wear

---
+++ android/wear/src/main/java/dev/jerson/safebox/AccountsActivity.kt
@@ -0,0 +1,48 @@
+package dev.jerson.safebox
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import android.view.View
+import android.widget.Toast
+import android.support.wear.widget.WearableLinearLayoutManager
+import android.support.wear.widget.WearableRecyclerView
+import dev.jerson.safebox.adapters.AccountAdapter
+import dev.jerson.safebox.grpc.Client
+import kotlinx.android.synthetic.main.activity_accounts.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class AccountsActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accounts)
+
+        load()
+
+        setAmbientEnabled()
+    }
+
+    private fun load() {
+
+        progressBar.visibility = View.VISIBLE
+        list.visibility = View.GONE
+
+        doAsync {
+            try {
+                val accounts = Client.connect().accounts
+
+                uiThread {
+                    list.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                    list.layoutManager = WearableLinearLayoutManager(this@AccountsActivity)
+                    list.adapter = AccountAdapter(accounts, this@AccountsActivity)
+
+                }
+
+            } catch (e: Exception) {
+                Toast.makeText(this@AccountsActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
