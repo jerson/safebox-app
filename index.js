41752f708023de571e6ca79e8b82c6bd2d82a10a
jeral17@gmail.com <Gerson Alexander Pardo Gamez>
2019-08-22 09:14:06 -0500

    added fastlane dir

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name } from './app.json';
 
+
+console.disableYellowBox = true;
 AppRegistry.registerComponent(name, () => App);
